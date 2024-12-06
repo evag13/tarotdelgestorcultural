88abbf4e1dee7f2d5d47fc2be50b823dc8335bd0
evag13 <evagadano@gmail.com>
2024-12-06 16:48:05 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let consejos = [];
+
+let tempConsejos = []; // Array temporal para manejar las frases sin repetición
+let consejoElegido = "clickea para hacerle una consulta al tarot del gestor cultural";
+
+
+function preload() {
+  // Cargar el archivo de texto
+  consejos = loadStrings('consejos.txt');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  textSize(12);
+  textWrap(WORD);
+  
+
+  // Inicializar el array temporal
+  reiniciarTempConsejos();
+}
+
+function draw() {
+  background(255);
+  
+  textSize(8);
+  fill (0);
+  textFont('Verdana');
+  text('cucurucho', 350, 375);
+  text('2024', 353, 388);
+  
+  textSize(12);
+  fill(255,0,0);
+  textFont('Courier New');
+  text(consejoElegido, width / 2, height / 2);
+}
+
+function mousePressed() {
+  if (tempConsejos.length === 0) {
+    // Si se han mostrado todos los consejos, reiniciar
+    reiniciarTempConsejos();
+  }
+
+  // Elegir un consejo al azar del array temporal
+  let index = int(random(tempConsejos.length));
+  consejoElegido = tempConsejos[index];
+
+  // Eliminar el consejo elegido del array temporal
+  tempConsejos.splice(index, 1);
+}
+
+function reiniciarTempConsejos() {
+  // Reiniciar el array temporal con todos los consejos
+  tempConsejos = consejos.slice(); // Copia del array original
+}
